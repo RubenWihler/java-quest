9eec954d96666a7cfbd69a97e46fd90035fed31a
wihler <wihlerruben@gmail.com>
2025-10-02 17:16:29 +0200

    base project

---
+++ src/main/java/io/github/RubenWihler/JavaQuest/App.java
@@ -0,0 +1,13 @@
+package io.github.RubenWihler.JavaQuest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("La beaute est la verite.");
+    }
+}
